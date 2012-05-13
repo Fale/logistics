ac77d889069aad92698f0973b9399c412e77560e
Fabio Alessandro Locati <flocati@grimp.eu>
2012-05-13 11:46:40 +0200

    Start working on lib/dhl tests

--- tests/libs/dhl.php
+++ tests/libs/dhl.php
@@ -1,13 +1,60 @@
 <?php
 class Dhl extends PHPUnit_Framework_TestCase
 {
-    public function testNewArrayIsEmpty()
+    public function testQuote()
     {
+        //$data['debug'] = 1;
+
+        $data['Username'] = "radius";
+        $data['Password'] = "c3Rm3ll!-";
+
+        $data['OriginPersonName'] = "Ferdinando Cermelli";
+        $data['OriginCompanyName'] = "Radius di Ferdinando Cermelli";
+        $data['OriginPhoneNumber'] = "+393482668888";
+        $data['OriginEmailAddress'] = "f.cermelli@snds.co";
+
+        $data['OrigStreetLine'] = "Via Tolstoj, 86";
+        $data['OrigCountryId'] = "IT";
+        $data['OrigPostal'] = "20098";
+        $data['OrigCity'] = "San Giuliano Milanese";
+
+        $data['OrigStreetLine'] = "Kemp House, 152-160 City Rd";
+        $data['OrigCountryId'] = "GB";
+        $data['OrigPostal'] = "EC1V 2NX";
+        $data['OrigCity'] = "London";
+
+        $data['DestPersonName'] = "Fabio Locati";
+        $data['DestCompanyName'] = "Ship and Sale Ltd";
+        $data['DestPhoneNumber'] = "+393482668888";
+        $data['DestEmailAddress'] = "f.cermelli@snds.co";
+
+        $data['DestStreetLine'] = "Via Olmo, 80";
+        $data['DestCountryId'] = "IT";
+        $data['DestPostal'] = "20090";
+        $data['DestCity'] = "Segrate";
+        $data['StateOrProvinceCode'] = "MI";
+
+        $data['DestStreetLine'] = "Kemp House, 152-160 City Rd";
+        $data['DestCountryId'] = "GB";
+        $data['DestPostal'] = "EC1V 2NX";
+        $data['DestCity'] = "London";
+        $data['StateOrProvinceCode'] = "Greater London";
+
+        $data['ShipTimeStamp'] = "2012-05-10T12:00:00GMT+01:00";
+        $data['Documents'] = "0";
+        //$data['AccountID'] = "128948309";
+        $data['AccountID'] = "105891642";
+        $data['CustomerReferences'] = "3-2012-1712";
+        $data['Packages']['1'] = Array( 'Weight' => '1.0', 'Length' => '21', 'Width' => '30', 'Height' => '6' );
+
+        $dhl = new Dhlws( $data );
+//        $d = $dhl->quote() ); 
+        //print_r( $dhl->shipment() ); */
         // Create the Array fixture.
         $fixture = array();
  
         // Assert that the size of the Array fixture is 0.
-        $this->assertEquals(0, sizeof($fixture));
+        $this->assertNotEmpty( $dhl->quote() );
     }
  
     public function testArrayContainsAnElement()
@@ -22,53 +69,4 @@ class Dhl extends PHPUnit_Framework_TestCase
         $this->assertEquals(1, sizeof($fixture));
     }
 }
-/*
-//$data['debug'] = 1;
-
-$data['Username'] = "radius";
-$data['Password'] = "c3Rm3ll!-";
-
-$data['OriginPersonName'] = "Ferdinando Cermelli";
-$data['OriginCompanyName'] = "Radius di Ferdinando Cermelli";
-$data['OriginPhoneNumber'] = "+393482668888";
-$data['OriginEmailAddress'] = "f.cermelli@snds.co";
-
-$data['OrigStreetLine'] = "Via Tolstoj, 86";
-$data['OrigCountryId'] = "IT";
-$data['OrigPostal'] = "20098";
-$data['OrigCity'] = "San Giuliano Milanese";
-
-$data['OrigStreetLine'] = "Kemp House, 152-160 City Rd";
-$data['OrigCountryId'] = "GB";
-$data['OrigPostal'] = "EC1V 2NX";
-$data['OrigCity'] = "London";
-
-
-$data['DestPersonName'] = "Fabio Locati";
-$data['DestCompanyName'] = "Ship and Sale Ltd";
-$data['DestPhoneNumber'] = "+393482668888";
-$data['DestEmailAddress'] = "f.cermelli@snds.co";
-
-$data['DestStreetLine'] = "Via Olmo, 80";
-$data['DestCountryId'] = "IT";
-$data['DestPostal'] = "20090";
-$data['DestCity'] = "Segrate";
-$data['StateOrProvinceCode'] = "MI";
-
-$data['DestStreetLine'] = "Kemp House, 152-160 City Rd";
-$data['DestCountryId'] = "GB";
-$data['DestPostal'] = "EC1V 2NX";
-$data['DestCity'] = "London";
-$data['StateOrProvinceCode'] = "Greater London";
-
-$data['ShipTimeStamp'] = "2012-05-10T12:00:00GMT+01:00";
-$data['Documents'] = "0";
-//$data['AccountID'] = "128948309";
-$data['AccountID'] = "105891642";
-$data['CustomerReferences'] = "3-2012-1712";
-$data['Packages']['1'] = Array( 'Weight' => '1.0', 'Length' => '21', 'Width' => '30', 'Height' => '6' );
-
-$dhl = new Dhlws( $data );
-print_r( $dhl->quote() ); 
-//print_r( $dhl->shipment() ); */
 ?>
