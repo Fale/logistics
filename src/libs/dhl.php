21d31da344fe7f4d6bfa25c5bfae5408c64f8bbf
Fabio Alessandro Locati <flocati@grimp.eu>
2012-05-13 11:56:48 +0200

    Fix the date undefined timezone

--- src/libs/dhl.php
+++ src/libs/dhl.php
@@ -9,6 +9,7 @@ class Dhlws
 
     public function Dhlws( $data )
     {
+        date_default_timezone_set('UTC');
         $this->data = $data;
         if( array_key_exists( 'debug', $data ) )
             $this->debug = $data['debug'];
